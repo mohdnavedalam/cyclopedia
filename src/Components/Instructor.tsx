91f40ddb0cdfef271ca7bcb6dc6f01f641a445c2
Mohd Naved Alam <mohdnavedalam1987@gmail.com>
2024-06-01 01:50:19 +0530

    separing out instructor - 2

--- src/Components/Instructor.tsx
+++ src/Components/Instructor.tsx
@@ -4,19 +4,29 @@ class Instructor extends React.Component<any, any> {
     constructor(props: any) {
         super(props);
     }
+    componentDidMount = () => { 
+        console.log("Instructor - Mounted");
+    };
+    componentDidUpdate = () => { 
+        console.log("Instructor - Updated");
+    };
+    componentWillUnmount = () => { 
+        console.log("Instructor - Unmounted");
+    };
     render() {
+        console.log("Instructor - Render");
         return (
             <div className="p-3">
-                        <span className="h4 text-success">Instructor</span>
-                        <i className="bi bi-toggle-ff btn btn-success btn-sm"></i>
-                        <br />
-                        Name: {this.props.instructor.name}
-                        <br />
-                        Email: {this.props.instructor.email}
-                        <br />
-                        Phone: {this.props.instructor.phone}
-                        <br />
-                    </div>
+                <span className="h4 text-success">Instructor</span>
+                <i className="bi bi-toggle-ff btn btn-success btn-sm"></i>
+                <br />
+                Name: {this.props.instructor.name}
+                <br />
+                Email: {this.props.instructor.email}
+                <br />
+                Phone: {this.props.instructor.phone}
+                <br />
+            </div>
         );
     }
 }
